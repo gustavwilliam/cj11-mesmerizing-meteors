280fb6b74c4583fa8fb8ceba9f9d852c6507bd9e
Noble47 <noblebest56@gmail.com>
2024-07-22 05:11:42 +0100

    Corrected formatting errors and fixed test assertion error

--- bot/tests/test_models.py
+++ bot/tests/test_models.py
@@ -1,50 +1,60 @@
 import unittest
 
-from database.database import Score
 from database.models import Score, ScoreSheet
 
 
 class TestDB:
-    def __init__(self):
+    """Fake Score repository."""
+
+    def __init__(self) -> None:
         self.store = []
 
-    def add(self, **kwargs):
+    def add(self, level: int, username: str, score: int) -> None:
+        """Add user to score board."""
+        kwargs = {"username": username, "level": level, "score": score}
         self.store.append(kwargs)
 
-    def fetch(self, level):
-        return [score for score in self.store if score['level'] == level]
+    def fetch(self, level: int) -> list[Score]:
+        """Fetch users from repository."""
+        return [score for score in self.store if score["level"] == level]
 
-    def _get(self, kwargs):
-        index = 0
-        for score in self.store:
-            if all((score[k] == kwargs[k]) for k in kwargs.keys()):
+    def _get(self, kwargs: dict) -> int:
+        """Get user index."""
+        for index, score in enumerate(self.store):
+            if all((score[k] == kwargs[k]) for k in kwargs):
                 return index
-            index += 1
+        return None
 
-    def get(self, **kwargs):
+    def get(self, level: int, username: str, score: int | None = None) -> Score:
+        """Get user detail."""
+        kwargs = {"level": level, "username": username}
+        if score:
+            kwargs["score"] = score
         index = self._get(kwargs)
         if index:
             return self.store[index]
+        return None
 
-    def update(self, **kwargs):
-        score = kwargs.pop('score')
+    def update(self, level: int, username: str, score: int) -> None:
+        """Update user score."""
+        kwargs = {"level": level, "username": username}
         index = self._get(kwargs)
         if index:
-            self.store[index]['score'] = score
+            self.store[index]["score"] = score
 
-    def remove(self, **kwargs):
+    def remove(self, level: int, username: str) -> None:
+        """Remove user."""
+        kwargs = {"level": level, "username": username}
         index = self._get(kwargs)
         if index:
             self.store.pop(index)
 
 
-class InMemoryDb(Score):
-    pass
-
-
 class TestModels(unittest.TestCase):
+    """Test class for models."""
 
-    def populate_db(self):
+    def populate_db(self) -> TestDB:
+        """Populate fake database with dummy data."""
         test_db = TestDB()
         test_db.add(username="noble", level=1, score=100)
         test_db.add(username="dejavu", level=2, score=125)
@@ -53,54 +63,69 @@ class TestModels(unittest.TestCase):
         test_db.add(username="verstergurkan", level=3, score=225)
         return test_db
 
-    def test_scoresheet_level(self):
-        test_db = self.populate_db()
-        scoresheet = ScoreSheet(db=test_db, level=2)
-        self.assertTrue(len(scoresheet) == 2)
+    def test_scoresheet_level(self) -> None:
+        """Test level property.
 
-    def test_score_in_scoresheet(self):
+        Check if the score row in scoresheet
+        object are those that belong to its
+        level.
+        """
+        test_db = self.populate_db()
+        rows = 2
+        scoresheet = ScoreSheet(db=test_db, level=2)
+        assert len(scoresheet) == rows
+
+    def test_score_in_scoresheet(self) -> None:
+        """Test score in scoresheet.
+
+        Check that a specific score is in scoresheet.
+        """
         test_db = self.populate_db()
         scoresheet = ScoreSheet(db=test_db, level=2)
         score_obj = Score(username="dejavu", level=2, score=125)
-        self.assertTrue(score_obj in scoresheet)
+        assert score_obj in scoresheet
 
-    def test_scoresheet_sorted(self):
+    def test_scoresheet_sorted(self) -> None:
+        """Test sort method."""
         test_db = self.populate_db()
         scoresheet = ScoreSheet(db=test_db, level=2)
         ordered_score_list = [
             Score(username="jasper", level=2, score=150),
-            Score(username="dejavu", level=2, score=125)
+            Score(username="dejavu", level=2, score=125),
         ]
 
-        scoresheet_list = [score for score in scoresheet.sort()]
-        self.assertTrue(ordered_score_list == scoresheet_list)
+        scoresheet_list = scoresheet.sort()
+        assert ordered_score_list == scoresheet_list
 
-    def test_scoresheet_add_saves_to_database(self):
+    def test_scoresheet_add_saves_to_database(self) -> None:
+        """Test scoresheet syncs with database."""
         test_db = self.populate_db()
         scoresheet = ScoreSheet(db=test_db, level=2)
         scoresheet.add(username="noble", score=100)
         assert test_db.get(username="noble", score=100, level=2) is not None
 
-    def test_update_scoresheet(self):
+    def test_update_scoresheet(self) -> None:
+        """Test update method."""
         test_db = self.populate_db()
         scoresheet = ScoreSheet(db=test_db, level=3)
-        scoresheet.update(username="heavenmercy", score=300)
-        
+        score = 300
+        scoresheet.update(username="heavenmercy", score=score)
+
         # check scoresheet
         score_obj = scoresheet.get("heavenmercy")
-        self.assertTrue(score_obj.score == 300)
-        
-        # check database 
-        row = test_db.get(username="heavenmercy", level=3)
-        self.assertTrue(row['score'] == 300)
+        assert score_obj.score == score
 
-    def test_remove_score_from_scoresheet(self):
+        # check database
+        row = test_db.get(username="heavenmercy", level=3)
+        assert row["score"] == score
+
+    def test_remove_score_from_scoresheet(self) -> None:
+        """Test remove method."""
         test_db = self.populate_db()
         scoresheet = ScoreSheet(db=test_db, level=1)
         score_obj = scoresheet.get(username="noble")
         scoresheet.remove(username="noble")
-        self.assertTrue(score_obj not in scoresheet)
-
+        assert score_obj not in scoresheet
 
 
 if __name__ == "__main__":
