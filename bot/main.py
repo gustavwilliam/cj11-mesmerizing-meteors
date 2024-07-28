dc9684d17bc5bd3894e916fb6f21e1d2123d3bde
Jaspe Michael <micaeljaspe@gmail.com>
2024-07-28 15:32:33 +0200

    Added /help command

--- bot/main.py
+++ bot/main.py
@@ -25,6 +25,39 @@ async def merhaba(interaction: discord.Interaction) -> None:
     await interaction.response.send_message(f"Pong! {interaction.user.display_name}", ephemeral=True)
 
 
+# Help slash command
+@bot.tree.command(name="help", description="Display the list of available commands.")
+async def help_command(interaction: discord.Interaction) -> None:
+    """Help command."""
+    embed = discord.Embed(
+        description="## Available commands",
+        color=discord.Color.blurple(),  # Set the color of the embed
+    )
+    embed.add_field(name="`/play`", value="Play the Python Adventures game", inline=False)
+
+    embed.add_field(name="", value="\n\n", inline=False)
+
+    embed.add_field(
+        name="`/eval <code>`",
+        value=('Runs the evaluator on a given <`code`>\n**Usage:** `/eval "code"` (code must be a snippet)'),
+        inline=False,
+    )
+    embed.add_field(name="", value="\n\n", inline=False)
+
+    embed.add_field(
+        name="`/level <x>`",
+        value=("Runs the game at level <`x`>\n**Usage:** `level x` (x must be an existing level)"),
+        inline=False,
+    )
+    embed.add_field(name="", value="\n\n", inline=False)
+
+    embed.add_field(name="`/help`", value="Displays this help message.", inline=False)
+
+    embed.set_thumbnail(url="https://i.imgur.com/e1tfdYP.png")
+    embed.set_author(name="Python Adventures")
+    await interaction.response.send_message(embed=embed)
+
+
 @bot.tree.command(name="play", description="Start the Python Adventures game!")
 async def get_map(interaction: discord.Interaction) -> None:
     """Start the game."""
